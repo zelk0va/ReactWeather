5ed1dab547ed3b0dfc0ae1488d8f5a94b34a57cb
Xiang <xiang.hcc@gmail.com>
2017-01-09 10:51:24 +0800

    Rename about page

--- public/bundle.js
+++ public/bundle.js
@@ -26574,9 +26574,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to the About page'
+	    )
 	  );
 	};
 
